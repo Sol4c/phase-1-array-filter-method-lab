4c20825f5e76c1a643facd94352d283b3aacd136
Sol4c <brownarmani86@yahoo.com>
2023-09-07 15:56:50 -0400

    35

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers,name){
+    const result = drivers.filter((driver) => driver.toUpperCase() == name.toUpperCase());
+
+    return result
+}
+    
+function fuzzyMatch(drivers, match){
+    const results = drivers.filter((driver)=> driver.toUpperCase().substring(0,2) === match.toUpperCase())
+    return results
+}
+
+function matchName(drivers, name){
+    
+const result = drivers.filter(driver => driver.name === name);
+
+return result
+}
